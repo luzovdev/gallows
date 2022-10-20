64a5afc290f74f4265e41ad80667798e078c5bb1
Web <web@it.step>
2022-10-20 20:10:09 +0300

    add fn-gameover

--- modules/showDashes.js
+++ modules/showDashes.js
@@ -1,10 +1,10 @@
 // Создать количество черточек взависимости от кол. букв
 import {dashesNode,dashes} from './const.js'
 
-function showRandomDashes(){
+export function showRandomDashes(){
     dashesNode.innerText = dashes.join(' ');
 }
-showRandomDashes()
+
 
 
 
