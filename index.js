70a84d075d744e17e15ad7638f7ccd24584c1260
Web <web@it.step>
2022-10-25 18:55:09 +0300

    fix/index.js

--- index.js
+++ index.js
@@ -1,34 +1,27 @@
-import {arrWordForRandom,btn,dashes,inputNode, dashesNode} from './modules/const.js';
-import {showRandomDashes} from './modules/showDashes.js';
-import {showErrorImage} from './modules/showErrorImage.js';
-import {victory} from './modules/victory.js';
+import { arrWordForRandom, btn, dashes, inputNode, dashesNode } from './modules/const.js';
+import { showRandomDashes } from './modules/showDashes.js';
+import { showErrorImage } from './modules/showErrorImage.js';
+import { victory } from './modules/victory.js';
 
-
-console.log(arrWordForRandom);
-console.log(dashes);
-
-
-function game(arrWord, arrDashes){ 
+function game(arrWord, arrDashes) {
     let countFalse = 0;
     showRandomDashes();
-    function onBtnClick () {
+    function onBtnClick() {
         arrWord.forEach((item, index) => {
-            if (inputNode.value === item) {
-                arrDashes[index] = item;
-                dashesNode.innerText.split(' ')[index] = inputNode.value;
-                showRandomDashes();
-                // console.log(arrDashes);
-            } 
-        });   
+            if (item === inputNode.value) {
+                dashes[index] = inputNode.value;
+                dashesNode.innerText = dashes.join(' ')
+            }
+        });
         if (!arrDashes.includes(inputNode.value)) {
             countFalse++
             showErrorImage(countFalse);
         }
         if (countFalse === 3) {
-                btn.removeEventListener('click', onBtnClick);
-            }    
-        victory(arrWord, arrDashes);            
+            btn.removeEventListener('click', onBtnClick);
+        }
+        victory(arrWord, arrDashes);
     }
-    btn.addEventListener('click', onBtnClick);      
+    btn.addEventListener('click', onBtnClick);
 }
 game(arrWordForRandom, dashes);
